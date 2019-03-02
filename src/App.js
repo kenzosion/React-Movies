16bd46a92dfe49b8c93e32de7949c9ab2fe90474
Haur Kang <haur.kang@gmail.com>
2019-03-02 23:26:37 +0000

    creates fourcolgrid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Actor from './components/elements/Actor/Actor';
+import FourColGrid from "./components/elements/FourColGrid/FourColGrid";
+
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Actor />
-      
+      <Actor />,
+      <FourColGrid />
     );
   }
 }
---
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function FourColGrid() {
+  return (
+    <div>
+      <h3>hello from fourcolgrid</h3>
+    </div>
+  )
+}
