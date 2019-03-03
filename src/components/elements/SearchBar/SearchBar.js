a69fe4cb8d3f6753dd312d79dddee6786de7e0a3
Haur Kang <haur.kang@gmail.com>
2019-03-03 22:39:56 +0000

    creates searchbar

---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import FontAwesome from 'react-fontawesome';
+import './SearchBar.css';
+
+export default class SearchBar extends Component {
+    state = {
+
+    }
+  render() {
+    return (
+      <div>
+        <h3>search bar</h3>
+      </div>
+    )
+  }
+}
