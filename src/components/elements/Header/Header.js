9f7594f170822e05ee0ed1ec6e0128de42b3b18b
Haur Kang <haur.kang@gmail.com>
2019-03-03 22:23:42 +0000

    initialise header component

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <React.Fragment>
+      <h3>hello from header component</h3>
+    </React.Fragment>
+  )
+}
