1bbd06881590b075885a1b89c2d8c3c199408f85
Haur Kang <haur.kang@gmail.com>
2019-03-04 21:54:07 +0000

    adds elements to home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react'
 import './Home.css';
+import HeroImage from '../elements/HeroImage/HeroImage';
+import SearchBar from '../elements/SearchBar/SearchBar';
+import FourColGrid from '../elements/FourColGrid/FourColGrid';
+import MovieThumb from '../elements/MovieThumb/MovieThumb';
+import LoadMoreBtn from '../elements/LoadMoreBtn/LoadMoreBtn';
+import Spinner from '../elements/Spinner/Spinner';
 
 export default class Home extends Component {
-
 state = {
 
 }
 
   render() {
     return (
-    <React.Fragment>
-        <h3>Hello from Home component</h3>
-    </React.Fragment>
-    )
+      <React.Fragment>
+        <div className="rmdb-home">
+          <HeroImage />
+          <SearchBar />
+          <FourColGrid />
+          <Spinner />
+          <LoadMoreBtn />
+        </div>
+      </React.Fragment>
+    );
   }
 }
--- src/components/elements/Spinner/Spinner.js
+++ src/components/elements/Spinner/Spinner.js
@@ -1,5 +1,5 @@
 import './Spinner.css';
-import React from 'react'
+import React from 'react';
 
 export default function Spinner() {
   return (
